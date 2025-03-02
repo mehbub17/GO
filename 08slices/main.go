4730c1a2bbfa48d398d5ab739952cf7d74aedd38
mehbub17 <mehbubhussain.17@gmail.com>
2025-03-02 17:25:17 +0530

    slices

---
+++ 08slices/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main(){
+	fmt.Println("Slices");
+
+	var flist = []string{"apple","mango","bananan"}
+
+	fmt.Printf("type of flist is %T\n",flist)
+
+	flist = append(flist,"coconut","sugarcane")
+
+	fmt.Println(flist)
+
+	var newList = flist[1:3]//start include end exclude
+	fmt.Println(newList)
+
+
+	newList = flist[1:]
+	fmt.Println(newList)
+
+
+	newList = append(newList,newList[1])
+	fmt.Println(newList)
+	fmt.Println(newList[:3])
+
+
+
+	highScore := make([]int,4) // also allocate memory and initialize it with 0
+	fmt.Printf("type %T\n",highScore)
+
+	highScore[0]=677;
+	highScore[1]=347;
+	highScore[2]=277;
+	highScore[3]=2277;
+	
+	fmt.Println(highScore)
+	// highScore[4]=2277;// error as out of bound
+
+	highScore = append(highScore, 33,22,444);
+	fmt.Println(highScore)
+
+	// as soon as append is used reallocation of memory happens
+
+	sort.Ints(highScore)
+	fmt.Println(highScore)
+	fmt.Println(sort.IntsAreSorted(highScore))//slice is sorted
+
+
+}
