d4f5205b824e280ca1f985ba1b06dc7c01005a92
mehbub17 <mehbubhussain.17@gmail.com>
2025-03-16 17:12:06 +0530

    making the getOne function for getting that course

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // creating the model
@@ -47,5 +49,30 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Get all the courses")
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(courses)
+	json.NewEncoder(w).Encode(courses) // w is the writer and inside encode whatever is json and will be thrown/given to user
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Get one courses")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab the id from request
+
+	params := mux.Vars(r) // params is set of key-val pairs using mux we can just extract them from request
+
+	// loop the course and find matching id and return the response
+
+	for _, course := range courses {
+
+		if course.CourseId == params["id"] { // params is k-v pair
+			json.NewEncoder(w).Encode(course.Author)
+			return
+
+		}
+	}
+
+	json.NewEncoder(w).Encode("<h2>No course found</h2>")
+	return
+
 }
